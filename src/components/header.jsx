dd40ae6db918f9b40f08ee8c1506a44eb287d874
Bhawaninayak25 <bhawaninayak1111@gmail.com>
2025-08-10 11:50:15 +0530

    portfolo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,11 +10,11 @@ const Header = () => {
     <header className="header">
       <div className="logo"> Myportfolio</div>
       <nav className={`nav ${menuOpen ? 'open' : ''}`}>
-        <a href="home">Home</a>
-        <a href="about">About</a>
-        <a href="skills">Skills</a>
-        <a href="projects">Projects</a>
-        <a href="contact">Contact</a>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
       </nav>
       <div className="menu-toggle" onClick={toggleMenu}>
         <span className="bar"></span>
